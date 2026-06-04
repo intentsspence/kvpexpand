6ce63bdd40cd18d24fb41af23dd35b7bb4f8098b
Stephen Spence <intentsspence.github@gmail.com>
2026-06-04 14:53:27 -0400

    Move typechecking responsibility from KVPExpander to parsers

--- src/kvpexpand/_types.py
+++ src/kvpexpand/_types.py
@@ -2,6 +2,6 @@ from typing import Callable
 
 ParsedPairs = dict[str, str]
 ExpandedPairs = dict[str, object]
-ValueParser = Callable[[str], ParsedPairs | None]
-ScalarParser = Callable[[str], object]
+ValueParser = Callable[[object], ParsedPairs | None]
+ScalarParser = Callable[[object], object]
 KeyGenerator = Callable[[str, str], str]
